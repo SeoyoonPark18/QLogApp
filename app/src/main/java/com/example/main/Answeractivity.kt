acd8ec9b89be55206f4f90db7c89f8c36f22b334
cho-sy <1208melon@naver.com>
2021-02-04 16:07:34 +0900

    qlog

--- app/src/main/java/com/example/main/Answeractivity.kt
+++ app/src/main/java/com/example/main/Answeractivity.kt
@@ -11,6 +11,7 @@ class Answeractivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answeractivity)
+        //efefefefe
 
     }
 }
