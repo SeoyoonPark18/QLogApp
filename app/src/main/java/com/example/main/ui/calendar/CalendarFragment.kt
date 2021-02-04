aa6b67e0b5e03017ee40ad2c3191db6b07780f51
YunHye-Mi <YunHye-Mi@users.noreply.github.com>
2021-02-04 18:27:12 +0900

    calendar

--- app/src/main/java/com/example/main/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/main/ui/calendar/CalendarFragment.kt
@@ -1,18 +1,22 @@
 package com.example.main.ui.calendar
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.main.Answeractivity
 import com.example.main.R
 
 class CalendarFragment : Fragment() {
 
     private lateinit var calendarViewModel: CalendarViewModel
+    lateinit var expansionButton: ImageButton
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +26,15 @@ class CalendarFragment : Fragment() {
         calendarViewModel =
                 ViewModelProvider(this).get(CalendarViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_calendar, container, false)
-        val textView: TextView = root.findViewById(R.id.text_calendar)
-        calendarViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        expansionButton = view.findViewById(R.id.writeButton)
+        expansionButton.setOnClickListener{
+            val intent = Intent(getActivity(), Answeractivity::class.java)
+            startActivity(intent)
+        }
     }
 }
