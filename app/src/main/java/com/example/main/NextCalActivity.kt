3be8c95054dd3f31a5950f376ca2c632864d501b
YunHye-Mi <YunHye-Mi@users.noreply.github.com>
2021-02-07 15:37:38 +0900

    calendar

--- app/src/main/java/com/example/main/NextCalActivity.kt
+++ app/src/main/java/com/example/main/NextCalActivity.kt
@@ -1,16 +1,10 @@
 package com.example.main
 
-import android.app.PendingIntent.getActivity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.ToggleButton
-import com.example.main.ui.calendar.CalendarFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlin.system.exitProcess
 
 class NextCalActivity : AppCompatActivity() {
     lateinit var closeButton: ImageButton
