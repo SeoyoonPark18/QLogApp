9535d132f2f0126faa7a0af7da48236b5cfe275b
YunHye-Mi <YunHye-Mi@users.noreply.github.com>
2021-02-07 16:22:03 +0900

    calendar

--- app/src/main/java/com/example/main/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/main/ui/calendar/CalendarFragment.kt
@@ -20,8 +20,7 @@ class CalendarFragment : Fragment() {
     private lateinit var calendarViewModel: CalendarViewModel
     lateinit var expansionButton: ImageButton
     lateinit var scrollText: ScrollView
-    lateinit var YearMonth: TextView
-    //lateinit var calendarView: CalendarView
+    lateinit var calendarView: CalendarView
     lateinit var dateView: TextView
 
     override fun onCreateView(
@@ -39,11 +38,9 @@ class CalendarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         expansionButton = view.findViewById(R.id.expansionButton)
         scrollText = view.findViewById(R.id.scrollText)
-        YearMonth = view.findViewById(R.id.YearMonth)
-        //calendarView = view.findViewById(R.id.calendarView)
+        calendarView = view.findViewById(R.id.calendarView)
         dateView = view.findViewById(R.id.dateView)
 
-        YearMonth.text = String.format("%d년 %d월", Calendar.YEAR+2020, Calendar.MONTH)
         dateView.text = String.format("%d년 %d월 %d일", Calendar.YEAR+2020, Calendar.MONTH, Calendar.DATE+2)
 
         expansionButton.setOnClickListener{
@@ -52,10 +49,10 @@ class CalendarFragment : Fragment() {
             startActivity(intent)
         }
 
-        /*calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
             dateView.visibility = View.VISIBLE
             dateView.text = String.format("%d년 %d월 %d일", year, month+1, dayOfMonth)
             //데이터 베이스
-        }*/
+        }
     }
 }
