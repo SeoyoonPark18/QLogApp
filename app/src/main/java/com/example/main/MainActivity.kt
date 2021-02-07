b31893fb2d8a7deab5ed2d2e27edd385c0a910cd
YunHye-Mi <YunHye-Mi@users.noreply.github.com>
2021-02-07 23:15:10 +0900

    MainActivity fragment 수정

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -1,34 +1,39 @@
 package com.example.main
 
 
-import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.main.ui.calendar.CalendarFragment
+import com.example.main.ui.friends.FriendsFragment
+import com.example.main.ui.home.HomeFragment
+import com.example.main.ui.settings.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class MainActivity : AppCompatActivity() {
-
-
-
+    lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_calendar,
-                R.id.navigation_friends,
-                R.id.navigation_settings
-            )
-        )
-        navView.setupWithNavController(navController)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        setCurrentFragment(HomeFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.navigation_home-> setCurrentFragment(HomeFragment())
+                R.id.navigation_calendar -> setCurrentFragment(CalendarFragment())
+                R.id.navigation_friends -> setCurrentFragment(FriendsFragment())
+                R.id.navigation_settings -> setCurrentFragment(SettingsFragment())
+            }
+            true
+        }
+
     }
+    private fun setCurrentFragment(fragment: Fragment)=
+            supportFragmentManager.beginTransaction().apply{
+                replace(R.id.flFragment,fragment)
+                commit()
+            }
 }
